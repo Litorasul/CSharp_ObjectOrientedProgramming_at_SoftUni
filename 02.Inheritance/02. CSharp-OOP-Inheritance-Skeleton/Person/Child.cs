7379955424187fc2eafec4252f5ea133066cb04d
Nikolay Kelov <n_kelov@yahoo.com>
2019-07-04 09:22:33 +0200

    Problem 01 - 66%

---
+++ 02.Inheritance/02. CSharp-OOP-Inheritance-Skeleton/Person/Child.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            :base(name, age)
+        {
+            if (age > 15)
+            {
+                throw new ArgumentOutOfRangeException("Child can not be older than 15!");
+            }
+        }
+    }
+}
---
+++ 02.Inheritance/02. CSharp-OOP-Inheritance-Skeleton/Person/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Age can not be negative!");
+                }
+                age = value;
+            }
+        }
+       
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
