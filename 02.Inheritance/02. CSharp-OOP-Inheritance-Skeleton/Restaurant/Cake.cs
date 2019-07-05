fefd395974e12ffecdf478dd49baafc09f3ab7e5
Nikolay Kelov <n_kelov@yahoo.com>
2019-07-05 14:18:28 +0300

    Problem 06 - 66%

---
+++ 02.Inheritance/02. CSharp-OOP-Inheritance-Skeleton/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public decimal CakePrice { get; set; }
+
+        public Cake(string name, decimal price, double grams = 250,
+            double calories = 1000, decimal cakePrice = 5)
+            :base(name, price, grams, calories)
+        {
+
+        }
+    }
+}
---
+++ 02.Inheritance/02. CSharp-OOP-Inheritance-Skeleton/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public double CoffeeMilliliters { get; set; }
+        public decimal CoffeePrice { get; set; }
+        public double Coffeine { get; set; }
+
+        public Coffee(string name, decimal price, double milliliters,
+            double coffeeMilliliters = 50, decimal coffeePrice = 3.50M)
+            :base(name, price, milliliters)
+        {
+            CoffeeMilliliters = coffeeMilliliters;
+            CoffeePrice = coffeePrice;
+        }
+    }
+}
