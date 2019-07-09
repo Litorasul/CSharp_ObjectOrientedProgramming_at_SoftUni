828c7c61f43676d47765fb5d8eee869cab29029e
Nikolay Kelov <n_kelov@yahoo.com>
2019-07-09 10:11:18 +0300

    Problem 01 - 75%

---
+++ 03.Encapsulation/03.Encapsulation/P01.ClassBoxData/Box.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ClassBoxData
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+        public double Lenght
+        {
+            get { return this.lenght; }
+            private set
+            {
+                ValidateInput(value, "Lenght");
+                lenght = value;
+            }
+        }
+        public double Width
+        {
+            get { return this.width; }
+            private set
+            {
+                ValidateInput(value, "Width");
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get { return this.height; }
+            private set
+            {
+                ValidateInput(value, "Height");
+                height = value;
+            }
+        }
+
+        public Box(double lenght, double width, double height)
+        {
+            this.Lenght = lenght;
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Volume()
+        {
+            double result = this.Lenght * this.Height * this.Width;
+            return result;
+        }
+        public double SurfaceArea()
+        {
+            double sideA = this.Lenght * this.Width;
+            double sideB = this.Lenght * this.Height;
+            double sideC = this.Width * this.Height;
+            double result = 2 * sideA + 2 * sideB + 2 * sideC;
+            return result;
+        }
+        public double LateralSurfaceArea()
+        {
+            double sideA = this.Lenght * this.Height;
+            double sideB = this.Width * this.Height;
+            double result = 2 * sideA + 2 * sideB;
+            return result;
+        }
+
+        private void ValidateInput(double value, string type)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{type} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ 03.Encapsulation/03.Encapsulation/P01.ClassBoxData/ClassBoxData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01.ClassBoxData
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box = new Box(lenght, width, height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.Volume():F2}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
