c96df4a02fac4b4a8a96755499691690ebd753f8
Nikolay Kelov <n_kelov@yahoo.com>
2019-07-16 17:06:20 +0300

    Problem 07 - 100%

---
+++ 04.InterfacesAndAbstraction/04.InterfacesAndAbstraction/P07.FoodShortage/Citizen.cs
@@ -0,0 +1,33 @@
+﻿namespace P07.FoodShortage
+{
+    public class Citizen : IBuyer
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public string Bitrhday { get; private set; }
+
+        public int Food { get; private set; }
+
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Bitrhday = birthday;
+            this.Food = 0;
+          
+        }
+
+        public override string ToString()
+        {
+            return this.Bitrhday;
+        }
+
+        public void BuyFood()
+        {
+            this.Food += 10;
+        }
+    }
+}
---
+++ 04.InterfacesAndAbstraction/04.InterfacesAndAbstraction/P07.FoodShortage/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.FoodShortage
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IBuyer> buyers = new List<IBuyer>();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+                if (info.Length == 4)
+                {
+                    string name = info[0];
+                    int age = int.Parse(info[1]);
+                    string id = info[2];
+                    string birthday = info[3];
+                    IBuyer citizen = new Citizen(name, age, id, birthday);
+                    buyers.Add(citizen);
+
+                }
+                else
+                {
+                    string name = info[0];
+                    int age = int.Parse(info[1]);
+                    string group = info[2];
+                    IBuyer rebel = new Rebel(name, age, group);
+                    buyers.Add(rebel);
+                }
+            }
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                IBuyer currentBuyer = buyers.Where(x => x.Name == command)
+                    .FirstOrDefault();
+
+                if (currentBuyer != null)
+                {
+                    currentBuyer.BuyFood();
+                }
+            }
+
+            int allFood = buyers.Sum(x => x.Food);
+            Console.WriteLine(allFood);
+        }
+    }
+}
