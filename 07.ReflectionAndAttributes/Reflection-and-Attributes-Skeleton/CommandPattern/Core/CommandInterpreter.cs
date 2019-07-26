e074e027621d2ef3b128554d8de5d4a8615c1e80
Nikolay Kelov <n_kelov@yahoo.com>
2019-07-26 12:56:12 +0300

    Problem 01 - 100%

---
+++ 07.ReflectionAndAttributes/Reflection-and-Attributes-Skeleton/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,44 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string CommandPostfix = "Command";
+        public string Read(string input)
+        {           
+            string[] commandInfo = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string commandName = commandInfo[0] + CommandPostfix;
+            string[] commandArgs = commandInfo
+                .Skip(1)
+                .ToArray();
+
+            Assembly assembly = Assembly
+                .GetCallingAssembly();
+
+            Type[] types = assembly.GetTypes();
+
+            Type typeToCreate = types
+                .FirstOrDefault(t => t.Name == commandName);
+
+            if (typeToCreate == null)
+            {
+                throw new InvalidOperationException("Invalid Command Type!");
+            }
+
+            Object instance = Activator.CreateInstance(typeToCreate);
+            ICommand command = (ICommand)instance;
+
+            string result = command.Execute(commandArgs);
+            return result;
+        }
+    }
+}
