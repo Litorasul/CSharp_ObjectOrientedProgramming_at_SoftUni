b559516e123c5b16ef94796ca81ae0eae6d56053
Nikolay Kelov <n_kelov@yahoo.com>
2019-08-02 13:58:18 +0300

    Problem 01 - 100%

--- 09.UnitTesting/09UnitTesting/Database.Tests/DatabaseTests.cs
+++ 09.UnitTesting/09UnitTesting/Database.Tests/DatabaseTests.cs
@@ -1,18 +1,74 @@
+using Database;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class DatabaseTests
     {
+        private Database.Database database;
         [SetUp]
         public void Setup()
         {
+            this.database = new Database.Database(new int[] { 1, 2 });
         }
 
         [Test]
-        public void Test1()
+        public void TestIfConstructorWorksCorrectly()
         {
-            Assert.Pass();
+            int expectedCount = 2;
+
+            Assert.That(this.database.Count, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void TestAddingCorrectly()
+        {
+            this.database.Add(3);
+            int expectedCount = 3;
+
+            Assert.That(this.database.Count, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void TestAddingOverTheLimit()
+        {
+            for (int i = 3; i <= 16; i++)
+            {
+                this.database.Add(i);
+            }
+
+            Assert.That(() => database.Add(1),
+                Throws.InvalidOperationException.With.Message.EqualTo("Array's capacity must be exactly 16 integers!"));
+
+        }
+
+        [Test]
+        public void TestRemoveCorrectly()
+        {
+            int expectedCount = 1;
+
+            this.database.Remove();
+
+            Assert.That(this.database.Count, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void TestRemoveFromEmptyDatabase()
+        {
+            this.database.Remove();
+            this.database.Remove();
+
+            Assert.That(() => this.database.Remove(),
+                Throws.InvalidOperationException.With.Message.EqualTo("The collection is empty!"));
+        }
+
+        [Test]
+        public void TestFetch()
+        {
+            int[] expected = new int[] { 1, 2 };
+            int[] actual = this.database.Fetch();
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
