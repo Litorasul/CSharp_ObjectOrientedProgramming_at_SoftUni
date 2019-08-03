f917a36c7ba086b39d1d6a0feeac28e54ab24172
Nikolay Kelov <n_kelov@yahoo.com>
2019-08-03 13:08:13 +0300

    Problem 04 - 100%

--- 09.UnitTesting/09UnitTesting/FightingArena.Tests/ArenaTests.cs
+++ 09.UnitTesting/09UnitTesting/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,74 @@
-﻿using NUnit.Framework;
+﻿using FightingArena;
+using NUnit.Framework;
+using System.Linq;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        Arena arena;
+
         [SetUp]
         public void Setup()
         {
+            Warrior pesho = new Warrior("Pesho", 6, 40);
+            Warrior gosho = new Warrior("Gosho", 5, 45);
+            arena = new Arena();
+            arena.Enroll(pesho);
+            arena.Enroll(gosho);
         }
 
         [Test]
-        public void Test1()
+        public void TestCountCorrectly()
         {
-            Assert.Pass();
+            int expectedCount = 2;
+            int actualCount = arena.Count;
+
+            Assert.That(actualCount, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void TestEnrollCorrectly()
+        {
+            string expectedName = "Pesho";
+            string actualName = arena.Warriors.Where(w => w.HP == 40).FirstOrDefault().Name;
+
+            Assert.That(actualName, Is.EqualTo(expectedName));
+        }
+
+        [Test]
+        public void TestEnrollAlreadyEnrolled()
+        {
+            Warrior peshoAgain = new Warrior("Pesho", 6, 40);
+
+            Assert.That(() => arena.Enroll(peshoAgain), Throws.InvalidOperationException
+                .With.Message.EqualTo("Warrior is already enrolled for the fights!"));
+        }
+
+        [Test]
+        public void TestFightCorrectly()
+        {
+            int expectedHP = 35;
+
+            arena.Fight("Gosho", "Pesho");
+            int actualHP = arena.Warriors.Where(w => w.Name == "Pesho").FirstOrDefault().HP;
+
+            Assert.That(actualHP, Is.EqualTo(expectedHP));
+        }
+
+        [Test]
+        public void TestFightWrongAttacker()
+        {
+            Assert.That(() => arena.Fight("Frank", "Gosho"), Throws.InvalidOperationException
+                .With.Message.EqualTo("There is no fighter with name Frank enrolled for the fights!"));
+        }
+
+        [Test]
+        public void TestFightWrongDefender()
+        {
+            Assert.That(() => arena.Fight("Gosho", "Frank"), Throws.InvalidOperationException
+                .With.Message.EqualTo("There is no fighter with name Frank enrolled for the fights!"));
         }
     }
 }
+
--- 09.UnitTesting/09UnitTesting/FightingArena.Tests/WarriorTests.cs
+++ 09.UnitTesting/09UnitTesting/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,133 @@
+using FightingArena;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private const int MIN_ATTACK_HP = 30;
+
+        Warrior warrior;
+
         [SetUp]
         public void Setup()
         {
+            warrior = new Warrior("Pesho", 5, 40);
         }
 
         [Test]
-        public void Test1()
+        public void TestNameCorrectly()
         {
-            Assert.Pass();
+            string expectedName = "Pesho";
+            string actualName = warrior.Name;
+
+            Assert.That(actualName, Is.EqualTo(expectedName));
+        }
+
+        [Test]
+        public void TestNameWithWhiteSpace()
+        {
+            string nameWhiteSpace = " ";
+
+            Assert.That(() => new Warrior(nameWhiteSpace, 3, 22), Throws.ArgumentException
+                .With.Message.EqualTo("Name should not be empty or whitespace!"));
+        }
+
+        [Test]
+        public void TestDamageCorrectly()
+        {
+            int expectedDamage = 5;
+            int actualDamage = warrior.Damage;
+
+            Assert.That(actualDamage, Is.EqualTo(expectedDamage));
+        }
+
+        [Test]
+        public void TestNegativeDamage()
+        {
+            Assert.That(() => new Warrior("g", -3, 22), Throws.ArgumentException
+                .With.Message.EqualTo("Damage value should be positive!"));
+        }
+
+        [Test]
+        public void TestHPCorrectly()
+        {
+            int expestedHP = 40;
+            int actualHP = warrior.HP;
+
+            Assert.That(actualHP, Is.EqualTo(expestedHP));
+        }
+
+        [Test]
+        public void TestNegativeHP()
+        {
+            Assert.That(() => new Warrior("k", 3, -22), Throws.ArgumentException
+                .With.Message.EqualTo("HP should not be negative!"));
+        }
+
+        [Test]
+        public void TestAttackCorrectlyAttackerHP()
+        {
+            Warrior defender = new Warrior("defender", 6, 40);
+            int expectedHP = 35;
+
+            warrior.Attack(defender);
+            int actualHP = defender.HP;
+
+            Assert.That(actualHP, Is.EqualTo(expectedHP));
+        }
+
+        [Test]
+        public void TestAttackCorrectlyDefenderHP()
+        {
+            Warrior defender = new Warrior("defender", 6, 40);
+            int expectedHP = 34;
+
+            warrior.Attack(defender);
+            int actualHP = warrior.HP;
+
+            Assert.That(actualHP, Is.EqualTo(expectedHP));
+        }
+
+        [Test]
+        public void TestAttackWithAttackerLowHP()
+        {
+            Warrior attacker = new Warrior("attacker", 6, 20);
+
+            Assert.That(() => attacker.Attack(warrior), Throws.InvalidOperationException
+                .With.Message.EqualTo("Your HP is too low in order to attack other warriors!"));
+        }
+
+        [Test]
+        public void TestAttackWithDefenderLowHP()
+        {
+            Warrior defender = new Warrior("defender", 6, 20);
+
+            Assert.That(() => warrior.Attack(defender), Throws.InvalidOperationException
+               .With.Message.EqualTo($"Enemy HP must be greater than {MIN_ATTACK_HP} in order to attack him!"));
+        }
+
+        [Test]
+        public void TestAttackWithStrongEnemy()
+        {
+            Warrior defender = new Warrior("defender", 55, 40);
+
+            Assert.That(() => warrior.Attack(defender), Throws.InvalidOperationException
+               .With.Message.EqualTo("You are trying to attack too strong enemy"));
+        }
+
+        [Test]
+        public void TestAttackWhenKill()
+        {
+            Warrior attacker = new Warrior("attacker", 50, 40);
+            int expectedHP = 0;
+
+            attacker.Attack(warrior);
+            int actualHP = warrior.HP;
+
+            Assert.That(actualHP, Is.EqualTo(expectedHP));
+            
+
         }
     }
 }
